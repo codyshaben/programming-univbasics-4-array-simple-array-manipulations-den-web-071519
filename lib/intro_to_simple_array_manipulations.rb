5e5abb2ea861ac07dfe8a834a4ab51e8f813ab5b
Cody Shaben <cshaben87@gmail.com>
2019-07-09 17:38:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,5 +46,5 @@ end
 
 def using_delete(instructors, "Steven")
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  instructors.delete(Steven)
+  instructors.delete("Steven")
 end
