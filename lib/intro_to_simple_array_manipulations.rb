e7938c9db0860d60f6fe1b73d4ec9a638d8e7ffe
Cody Shaben <cshaben87@gmail.com>
2019-07-09 16:52:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,6 +31,5 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(list_of_programming_languages, another_language)
-  another_language = [4]
   list_of_programming_languages.insert(another_language)
 end
