cb75c4645f9f9fbc45baa2e6ef6a083d307c8425
Cody Shaben <cshaben87@gmail.com>
2019-07-09 17:44:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -44,7 +44,7 @@ def using_flatten(instruments)
   instruments.flatten
 end
 
-def using_delete(instructors, "Steven")
+def using_delete(instructors, "Steven" )
   instructors.delete("Steven")
   no_offense_steven = using_delete(instructors, "Steven")
 end
